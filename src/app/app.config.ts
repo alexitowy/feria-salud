93ee188332755f5aa9692793e13606b8ea8ec5ba
Alex Guerrero <alexitowy@gmail.com>
2025-04-15 17:36:16 +0200

    feat: integrate Firebase and Tailwind CSS into the Angular application
    - Added Firebase dependencies to package.json for Firestore integration.
    - Configured Firebase initialization in app.config.ts with environment variables.
    - Created environment.ts file to store Firebase configuration.
    - Updated styles.scss to include Tailwind CSS directives.
    - Added tailwind.config.js for Tailwind CSS configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../enviroments/environment';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAnimationsAsync(),
+    
+  ]
 };
