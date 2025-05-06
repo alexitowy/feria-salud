c8d6ec4a708c86b0e29c83f7660cdfb13c80c633
Alex Guerrero <alexitowy@gmail.com>
2025-05-06 13:51:53 +0200

    feat: implement OnInit lifecycle hook to clear storage on thank you page

--- src/app/pages/participant/thank-you/thank-you.component.ts
+++ src/app/pages/participant/thank-you/thank-you.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { StorageService } from '../../../core/services/storage.service'
 
 @Component({
   selector: 'app-thank-you',
   imports: [],
   templateUrl: './thank-you.component.html',
-  styleUrl: './thank-you.component.scss'
+  styleUrl: './thank-you.component.scss',
 })
-export class ThankYouComponent {
+export class ThankYouComponent implements OnInit {
+  constructor(private readonly storegeService: StorageService) {}
 
+  ngOnInit(): void {
+    this.storegeService.clearAll()
+  }
 }
